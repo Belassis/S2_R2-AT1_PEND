c2ebebbb5df0d0ad7ebe574f270559da52935005
Isinha <isabelly.assis.5261@gmail.com>
2026-03-13 12:15:23 -0300

    Atividade1

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-let Additem = document.getElementById("Additem");
-let btnAdicionar = document.getElementById("btnAdicionar");
-let itemEscirto
-
-let item = document.querySelector(".Lista");
-let novoItem
+let inputTarefa = document.getElementById("inputTarefa");
+let btnAdicionar = document.querySelector("#btnAdicionar");
+let mensagem = document.getElementById("mensagem");
+let listaTarefas = document.getElementById("listaTarefas");
 
 btnAdicionar.addEventListener("click", () => {
-        itemEscirto = Additem.value
 
-        if (itemEscirto) {
-         novoItem = document.createElement("li");
-         novoItem.innerText = itemEscirto;
-         item.appendChild(novoItem)
-        }
+    if (inputTarefa.value.trim() === "") {
+        mensagem.innerText = 'A lista está vazia!';
+        mensagem.className = 'text-danger fw-bold';
+    } else {
+
+        let novoItem = document.createElement("li");
+        novoItem.innerText = inputTarefa.value;
+        novoItem.className = "list-group-item";
+
+        listaTarefas.appendChild(novoItem);
+
+        mensagem.innerText = 'Item adicionado com sucesso!';
+        mensagem.className = 'text-success fw-bold';
+
+        inputTarefa.value = "";
+    }
 
 });
