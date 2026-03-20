485e184e7a944b67d751a9d50d839813b4683df9
Isinha <isabelly.assis.5261@gmail.com>
2026-03-20 08:15:14 -0300

    atvd6

--- script.js
+++ script.js
@@ -7,25 +7,46 @@ btnAdicionar.addEventListener("click", () => {
 
     if (inputTarefa.value == "") {
 
-         mensagem.innerHTML = '<button type="button" class="btn btn-outline-danger">A lista está vazia!</button>';
+        mensagem.innerHTML = '<button type="button" class="btn btn-outline-danger">A lista está vazia!</button>';
 
     } else {
 
         let novaTarefa = document.createElement("li");
         novaTarefa.className = "list-group-item";
-        novaTarefa.innerText = inputTarefa.value;
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.style.marginRight = "10px";
+
+
+        let texto = document.createTextNode(inputTarefa.value);
 
         let btnRemover = document.createElement("button");
         btnRemover.innerText = "Remover";
         btnRemover.className = "btn btn-danger btn-sm";
+        btnRemover.style.marginLeft = "10px";
 
         btnRemover.addEventListener("click", () => {
             novaTarefa.remove();
         });
+
+        // risca a tarefa quando marcar
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                novaTarefa.style.textDecoration = "line-through";
+            } else {
+                novaTarefa.style.textDecoration = "none";
+            }
+        });
+
+        // adiciona tudo no li
+        novaTarefa.appendChild(checkbox);
+        novaTarefa.appendChild(texto);
         novaTarefa.appendChild(btnRemover);
+
         listaTarefas.appendChild(novaTarefa);
 
-              mensagem.innerHTML = '<button type="button" class="btn btn-outline-success">Item adicionado com sucesso</button>';
+        mensagem.innerHTML = '<button type="button" class="btn btn-outline-success">Item adicionado com sucesso</button>';
 
         inputTarefa.value = "";
     }
